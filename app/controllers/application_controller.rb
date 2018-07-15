bfe35edc26f0872578c92c902608b7b93703f68a
Veronica Bustabad <vbustabad@gmail.com>
2018-07-15 20:59:00 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,6 +17,7 @@ class ApplicationController < Sinatra::Base
 
   post '/registrations' do
     @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+    @user.save
 
     redirect '/users/home'
   end
@@ -30,7 +31,7 @@ class ApplicationController < Sinatra::Base
     redirect '/users/home'
   end
 
-  get '/sessions/logout' do 
+  get '/sessions/logout' do
 
     redirect '/'
   end
